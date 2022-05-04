db8f0a2ad930bd570a4c85180c1400613788dd4f
Simo <szymon.333@wp.pl>
2022-05-04 18:15:20 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("ZADANIE 1");
+let hello = "hello";
+let js = "JS";
+let text = hello + " " + js;
+
+console.log(text);
+
+let number = 15;              //number -> 15
+let txt = "txt";              //string -> txt
+let numbAndTxt = number+text; //string -> 15hello JS
+let bool = true;              //boolean -> true
+let spec = undefined;         //undefinded -> undefined
+
+console.log("ZADANIE 2");
+console.log(typeof spec);
+console.log(spec);
+
+
