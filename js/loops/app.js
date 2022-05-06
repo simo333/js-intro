27858bb860ec55e071c418806fbd9ad42172bfce
Simo <szymon.333@wp.pl>
2022-05-06 22:01:54 +0200

    Tasks: JS - loops

--- js/loops/app.js
+++ js/loops/app.js
@@ -1,6 +1,16 @@
+console.log("Zadanie 1");
 let input = prompt("Z jakiej liczby chcesz obliczyć silnię?");
 let result = 1;
-for (i = 1; i <= input; i++) {
+for (let i = 1; i <= input; i++) {
   result = result * i;
 }
 console.log(result);
+
+console.log("Zadanie 2");
+let x = prompt("x =");
+let y = prompt("y =");
+let result2 = Number(x);
+for (++x; x <= y; x++) {
+  result2 = result2 + x;
+}
+console.log(result2);
