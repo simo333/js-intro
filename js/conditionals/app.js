5dead57c7acc8679cc17c2d0131644351c79bbe1
Simo <szymon.333@wp.pl>
2022-05-06 21:31:52 +0200

    Tasks: JS - conditional statements II

--- js/conditionals/app.js
+++ js/conditionals/app.js
@@ -1,5 +1,23 @@
+console.log("Zadanie 1");
 let input = prompt("Ile masz lat?");
-
 let txt = input >= 18 ? "Jesteś pełnoletni" : "Jesteś niepełnoletni";
-
 console.log(txt);
+
+console.log("Zadanie 2");
+let grade = prompt("Podaj ocene 1-6:");
+console.log(grade)
+switch (grade) {
+  case '1': console.log("niedostateczny");
+          break;
+  case '2': console.log("dopuszczjący");
+          break;
+  case '3': console.log("dostateczny");
+          break;
+  case '4': console.log("dobru");
+          break;
+  case '5': console.log("bardzo dobry");
+          break;
+  case '6': console.log("celujący");
+          break;
+  default: console.log("Nie ocena")
+}
